694f3d13902767108ad295947b9ca77d07e42845
Tallis Larsen <tallisblarsen@gmail.com>
2024-06-21 09:10:48 -0500

    literally made the entire mod in this one commit lol - Only affects mobGriefing and playersleepingpercentage for now

--- src/dllmain.cpp
+++ src/dllmain.cpp
@@ -1,17 +1,22 @@
-﻿#include "dllmain.hpp"
+﻿#include <iostream>
+#include "dllmain.hpp"
+#include "minecraft/src/common/world/level/storage/GameRules.hpp"
 
-// Ran when the mod is loaded into the game by AmethystRuntime
-ModFunction void Initialize(AmethystContext* ctx) 
-{
-    // Logging from <Amethyst/Log.h>
-    Log::Info("Hello, Amethyst World!");
+SafetyInlineHook _GameRules__registerRules;
 
-    // Add a listener to a built in amethyst event
-    ctx->mEventManager.onStartJoinGame.AddListener(&OnStartJoinGame);
+void GameRules_registerRules(GameRules* self) {
+  _GameRules__registerRules.call<void>(self);
+  for (int i = 0; i < self->gamerules.size(); i++) {
+    if (self->gamerules[i].name == "mobGriefing" || self->gamerules[i].name == "playersSleepingPercentage")
+      self->gamerules[i].requiresCheats = false;
+  }
 }
 
-// Subscribed to amethysts on start join game event in Initialize
-void OnStartJoinGame(ClientInstance* client)
-{
-    Log::Info("The player has joined the game!");
+ModFunction void Initialize(AmethystContext* ctx) {
+  HookManager& hooks = ctx->mHookManager;
+
+  hooks.RegisterFunction<&GameRules::_registerRules>("48 89 5C 24 ? 48 89 74 24 ? 48 89 7C 24 ? 55 48 8D AC 24 ? ? ? ? 48 81 EC ? ? ? ? 48 8B F9 48 83 C1");
+  hooks.CreateHook<&GameRules::_registerRules>(_GameRules__registerRules, &GameRules_registerRules);
+
+  Log::Info("[CheatlessTweaks] Mod successfully initialized!");
 }
